35bb6b0215dbb3d72d110d8d7207bb5e351f27c9
Sarmad rafique <sarmadrafique040@gmail.com>
2023-01-28 22:01:58 +0500

    Added an array of typescript

---
+++ dist/array.js
@@ -0,0 +1,6 @@
+"use strict";
+let number = [1, 2, 3];
+let sum = 0;
+number.map(e => sum += e);
+console.log("Sum of array: " + sum);
+//# sourceMappingURL=array.js.map
---
+++ src/array.ts
@@ -0,0 +1,4 @@
+let number:number[] = [1,2,3];
+ let sum=0;
+ number.map(e=>sum+=e)
+ console.log("Sum of array: " + sum);
