69777f763d62836fe80757515c1d6eb65910ae79
Sarmad rafique <sarmadrafique040@gmail.com>
2023-03-10 21:40:21 +0500

    Check Character Vowel or consonant

---
+++ src/exercise/vowel consonant.ts
@@ -0,0 +1,11 @@
+// type vowel = "a" | "e" | "i" | "o" | "u";
+let character = "E";
+if (character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u') {
+    console.log('LowerCase Vowel: ');
+}
+else if (character == 'A' || character <= 'E' || character == 'I' || character == 'O' || character == 'U') {
+    console.log('UpperCase Vowel');
+}
+else {
+    console.log('Consonant Character: ');
+}
